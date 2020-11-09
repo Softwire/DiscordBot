255dc9f91242eb46930d40f7900fc870e5dc3eca
Shivam <Shivam.Chandarana@softwire.com>
2020-11-09 10:54:02 +0000

    Create Bot Skeleton

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        private DiscordSocketClient _client;
+
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            
+            _client.Log += Log;
+            
+            var token = ""; // Insert token here locally. Don't push with it in
+            
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
