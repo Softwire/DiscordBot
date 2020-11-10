93c3d853c701069b305da2cfd4965a5635fc467d
FraMa <franky.ma24@gmail.com>
2020-11-10 12:19:19 +0000

    Add constructor for Signup class

--- DiscordBot/Models/Signup.cs
+++ DiscordBot/Models/Signup.cs
@@ -4,5 +4,11 @@
     {
         public string Id;
         public string Role;
+
+        public Signup(string id, string role)
+        {
+            Id = id;
+            Role = role;
+        }
     }
 }
