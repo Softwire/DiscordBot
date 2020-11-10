995b29b3931f79a3421b808572153e07296da97e
Benji Marshall <Benji.Marshall@softwire.com>
2020-11-10 11:53:39 +0000

    Add a basic interface with Google Sheets API
    A demo of reading and writing from a private Google Sheet. Nice credential
    storage has not been set up nicely yet, credentials and access to the private
    Google Sheet can be provided if you DM me on Slack.

---
+++ DiscordBot.DataAccess/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace DiscordBot.DataAccess
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var service = new EventsSheetsService();
+            await service.StartService();
+
+            await service.ReadColumns();
+
+            await service.WriteRow();
+        }
+    }
+}
