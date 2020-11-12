bf55cbc9cab6b999d9ead92f72d6a0946c5b8dbb
Benji Marshall <Benji.Marshall@softwire.com>
2020-11-12 10:16:59 +0000

    Remove key to row dictionary, tidy up
    Remove the key to row dictionary, with the plan being to use a search/
    update approach to editing events so all state is stored in the Sheet,
    and not cached in a dictionary here that needs to be maintained.

--- DiscordBot.DataAccess/EventsSheetsService.cs
+++ DiscordBot.DataAccess/EventsSheetsService.cs
@@ -31,7 +31,6 @@ namespace DiscordBot.DataAccess
 
         private readonly SheetsService sheetsService;
         private int largestKey;
-        private readonly Dictionary<int, int> keyToRowNumberMap;
 
         public EventsSheetsService()
         {
@@ -43,36 +42,7 @@ namespace DiscordBot.DataAccess
                 ApplicationName = applicationName
             });
 
-            try
-            {
-                var request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, "EventsMetadata!A:A");
-                var response = request.Execute();
-
-                if (response == null || response.Values.Count < 1)
-                {
-                    throw new EventsSheetsInitialisationException("Metadata sheet is empty");
-                }
-
-                // Find which row each event is on, and the largest key in use
-                keyToRowNumberMap = new Dictionary<int, int>();
-                var keys = response.Values.Skip(1);
-                largestKey =
-                    keys
-                    .Select((keyRow, rowNumber) =>
-                        {
-                            var key = int.Parse((string) keyRow[0]);
-                            keyToRowNumberMap.Add(key, rowNumber + 1);
-                            return key;
-                        })
-                    .Max();
-            }
-            catch (GoogleApiException exception)
-            {
-                throw new EventsSheetsInitialisationException(
-                    $"Events Sheets Service couldn't initialise",
-                    exception
-                );
-            }
+            largestKey = GetLargestKey();
         }
 
         public async Task ReadColumns()
@@ -143,14 +113,7 @@ namespace DiscordBot.DataAccess
             );
             request.ValueInputOption = AppendRequest.ValueInputOptionEnum.RAW;
 
-            var response = await request.ExecuteAsync();
-
-            // Record which row the new event was inserted on
-            keyToRowNumberMap.Add(
-                largestKey,
-                GetRowNumberFromRange(response.Updates.UpdatedRange)
-            );
-            return;
+            await request.ExecuteAsync();
         }
 
         public async Task EditEventAsync(
@@ -188,11 +151,33 @@ namespace DiscordBot.DataAccess
                 .UnderlyingCredential as ServiceAccountCredential;
         }
 
-        private int GetRowNumberFromRange(string range)
+        private int GetLargestKey()
         {
-            const string pattern = @"^EventsMetadata!A\d+:E(\d+)$";
-            var rowString = Regex.Match(range, pattern).Groups[1].ToString();
-            return int.Parse(rowString);
+            try
+            {
+                var request = sheetsService.Spreadsheets.Values.Get(spreadsheetId, "EventsMetadata!A:A");
+                var response = request.Execute();
+
+                if (response == null || response.Values.Count < 1)
+                {
+                    throw new EventsSheetsInitialisationException("Metadata sheet is empty");
+                }
+
+                // If the table only contains headers, and no data
+                if (response.Values.Count == 1)
+                {
+                    return 0;
+                }
+
+                return int.Parse((string)response.Values.Skip(1).Last()[0]);
+            }
+            catch (GoogleApiException exception)
+            {
+                throw new EventsSheetsInitialisationException(
+                    $"Events Sheets Service couldn't initialise",
+                    exception
+                );
+            }
         }
     }
 }
