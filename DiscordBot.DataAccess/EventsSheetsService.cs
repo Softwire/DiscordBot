7bc839313626309014f9045a23df66e5c4240287
Benji Marshall <benji.marshall@softwire.com>
2020-11-17 15:55:53 +0000

    Add responses for user (#32)
    * Add method to get potential responses to an event
    * Move SheetsService exceptions to own folder
    * Tidy up and generalise finding rows in sheets
    * Implement adding responses to an event

--- DiscordBot.DataAccess/EventsSheetsService.cs
+++ DiscordBot.DataAccess/EventsSheetsService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DiscordBot.DataAccess.Exceptions;
 using Google;
 using DiscordBot.DataAccess.Models;
 using Google.Apis.Auth.OAuth2;
@@ -57,6 +58,8 @@ namespace DiscordBot.DataAccess
         private readonly SheetsColumn TimeColumn = new SheetsColumn(3);
         private readonly SheetsColumn TimeZoneColumn = new SheetsColumn(4);
         private readonly SheetsColumn MessageIdColumn = new SheetsColumn(5);
+
+        private readonly SheetsColumn UserIdColumn = new SheetsColumn(0);
 
         public EventsSheetsService()
         {
@@ -254,11 +257,26 @@ namespace DiscordBot.DataAccess
             }
         }
 
-#pragma warning disable 1998 // Disable compiler warning stating that the unimplemented functions are synchronous
         public async Task AddResponseForUserAsync(int eventKey, ulong userId, string responseEmoji)
         {
+            var responseRowTask = GetResponseRowNumberAsync(eventKey, userId);
+            var responseColumnsTask = GetEventResponseOptionsAsync(eventKey);
+
+            await Task.WhenAll(responseRowTask, responseColumnsTask);
+            var responseRow = responseRowTask.Result;
+            var responseColumns = responseColumnsTask.Result;
+
+            if (responseRow == null)
+            {
+                await AddResponseForNewUserAsync(eventKey, userId, responseEmoji, responseColumns);
+            }
+            else
+            {
+                await AddResponseForExistingUserAsync(eventKey, responseRow.Value, responseEmoji, responseColumns);
+            }
         }
 
+#pragma warning disable 1998 // Disable compiler warning stating that the unimplemented functions are synchronous
         public async Task ClearResponsesForUserAsync(int eventKey, ulong userId)
         {
         }
@@ -372,40 +390,96 @@ namespace DiscordBot.DataAccess
             }
         }
 
-        private async Task<int> GetEventRowNumber(int eventKey)
+        private async Task<int?> GetRowNumberFromKeyAsync(
+            string sheetName,
+            SheetsColumn keyColumn,
+            int numberOfHeaderRows,
+            ulong key
+        )
         {
             try
             {
                 var request = sheetsService.Spreadsheets.Values.Get(
                     spreadsheetId,
-                    $"{MetadataSheetName}!{KeyColumn.Letter}:{KeyColumn.Letter}"
+                    $"{sheetName}!{keyColumn.Letter}:{keyColumn.Letter}"
                 );
                 var response = await request.ExecuteAsync();
 
-                if (response == null || response.Values.Count < 2)
+                if (response == null || response.Values.Count < numberOfHeaderRows)
                 {
-                    throw new EventNotFoundException($"Event key {eventKey} not recognised");
+                    throw new EventNotFoundException($"Event key {key} not recognised");
                 }
 
                 var rowNumber = response.Values
-                    .Skip(1)  // Skip header row
+                    .Skip(numberOfHeaderRows)
                     .Select((values, index) => (values, index))
-                    .First(row => int.Parse((string)row.values[KeyColumn.Index]) == eventKey);
+                    .First(row => ulong.Parse((string)row.values[keyColumn.Index]) == key);
 
-                // Extract row number, plus 2 to correct for two this:
+                // Extract row number, plus a correction factor:
+                // Correct for skipping the header
                 // These lists are 0 indexed, but Sheets index from 1
-                // Correct for skipping row 1, the header
-                return rowNumber.index + 2;
+                return rowNumber.index + numberOfHeaderRows + 1;
             }
             catch (InvalidOperationException)
             {
+                return null;
+            }
+        }
+
+        private async Task<int> GetEventRowNumber(int eventKey)
+        {
+            var rowNumber = await GetRowNumberFromKeyAsync(MetadataSheetName, KeyColumn, 1, (ulong) eventKey);
+            if (rowNumber == null)
+            {
                 throw new EventNotFoundException($"Event key {eventKey} not recognised");
+            }
+
+            return rowNumber.Value;
+        }
+
+        private async Task<int?> GetResponseRowNumberAsync(int eventKey, ulong userKey)
+        {
+            try
+            {
+                return await GetRowNumberFromKeyAsync(eventKey.ToString(), UserIdColumn, 2, userKey);
             }
             catch (GoogleApiException exception)
             {
-                throw new EventsSheetsInitialisationException(
-                    "Events Sheets Service couldn't initialise",
+                throw new EventInitialisationException(
+                    $"Sign ups have not been released for event {eventKey}",
                     exception
+                );
+            }
+        }
+
+        private async Task<IEnumerable<EventResponse>> GetEventResponseOptionsAsync(int eventKey)
+        {
+            try
+            {
+                var request = sheetsService.Spreadsheets.Values.Get(
+                    spreadsheetId,
+                    $"{eventKey}!1:2"
+                );
+                request.ValueRenderOption = GetRequest.ValueRenderOptionEnum.FORMATTEDVALUE;
+
+                var sheetsResponse = await request.ExecuteAsync();
+
+                if (sheetsResponse == null || sheetsResponse.Values.Count < 2)
+                {
+                    throw new EventsSheetsInitialisationException($"Event sheet {eventKey} is empty");
+                }
+
+                return sheetsResponse.Values[0]
+                    .Zip(sheetsResponse.Values[1])
+                    .Skip(1) // Skip titles column
+                    .Select<(object emoji, object name), EventResponse>(response =>
+                        new EventResponse((string)response.emoji, (string)response.name)
+                    );
+            }
+            catch (GoogleApiException)
+            {
+                throw new EventInitialisationException(
+                    $"Could not find event responses for event {eventKey}. Has it been published yet?"
                 );
             }
         }
@@ -438,6 +512,52 @@ namespace DiscordBot.DataAccess
                 Range = range,
                 Values = new IList<object>[] { new[] { value } }
             };
+        }
+
+        private async Task AddResponseForNewUserAsync(
+            int eventKey,
+            ulong userId,
+            string responseEmoji,
+            IEnumerable<EventResponse> responseColumns
+        )
+        {
+            var newRow = responseColumns
+                .Select(response => response.Emoji == responseEmoji ? 1 : 0)
+                .Cast<object>()
+                .Prepend(userId);
+
+            var values = new ValueRange()
+            {
+                Values = new IList<object>[]
+                {
+                    newRow.ToList()
+                }
+            };
+
+            var request = sheetsService.Spreadsheets.Values.Append(values, spreadsheetId, $"{eventKey}");
+            request.ValueInputOption = AppendRequest.ValueInputOptionEnum.RAW;
+
+            await request.ExecuteAsync();
+        }
+
+        private async Task AddResponseForExistingUserAsync(
+            int eventKey,
+            int responseRow,
+            string responseEmoji,
+            IEnumerable<EventResponse> responseColumns
+        )
+        {
+            var index = responseColumns.ToList().FindIndex(responseOption => responseOption.Emoji == responseEmoji);
+
+            // Plus 1 to correct for the title column that is skipped over and not in the response list.
+            var updateColumn = new SheetsColumn(index + 1);
+            var range = $"{eventKey}!{updateColumn.Letter}{responseRow}";
+
+            var cellValue = MakeCellUpdate(range, 1);
+            var request = sheetsService.Spreadsheets.Values.Update(cellValue, spreadsheetId, range);
+            request.ValueInputOption = UpdateRequest.ValueInputOptionEnum.RAW;
+
+            await request.ExecuteAsync();
         }
     }
 }
---
+++ DiscordBot.DataAccess/Exceptions/EventInitialisationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DiscordBot.DataAccess.Exceptions
+{
+    class EventInitialisationException : Exception
+    {
+        public EventInitialisationException(string? message)
+        : base(message)
+        {
+        }
+
+        public EventInitialisationException(string? message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
