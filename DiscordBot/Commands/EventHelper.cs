7b284832f8a387fd510394baf3801fa36dded997
kwunkiuma <36454654+kwunkiuma@users.noreply.github.com>
2020-11-18 16:53:16 +0000

    Add reaction handling (#40)
    * Add reaction handling
    
    * Add comment for bot ignoring own reactions
    
    * Rename variables and add newlines for readability
    
    * Split reaction handler method
    
    * Remove redundant import
    
    * Resolve service instances
    
    * Add EventHelper to EventCommands constructor
    
    * Change to pass service as parameter when needed
    
    * Remove redundant static variable
    
    * Fix eventsSheetsService
    
    * Add comment to skipping own reactions

---
+++ DiscordBot/Commands/EventHelper.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiscordBot.DataAccess;
+using DiscordBot.DataAccess.Exceptions;
+using DiscordBot.DataAccess.Models;
+using DSharpPlus.Entities;
+
+namespace DiscordBot.Commands
+{
+    class EventHelper
+    {
+        public static async Task<DiscordEvent?> GetEventFromMessageIdOrDefaultAsync(ulong messageId, IEventsSheetsService eventsSheetsService)
+        {
+            try
+            {
+                return await eventsSheetsService.GetEventFromMessageIdAsync(messageId);
+            }
+            catch (EventNotFoundException)
+            {
+                return null;
+            }
+        }
+
+        public static Dictionary<ulong, IEnumerable<EventResponse>> GetResponsesByUser(
+            Dictionary<EventResponse, IEnumerable<ulong>> signupsByResponse)
+        {
+            var signupsByUser = new Dictionary<ulong, IEnumerable<EventResponse>>();
+
+            var userIds = signupsByResponse.Values
+                .SelectMany(responseSignups => responseSignups)
+                .Distinct();
+
+            foreach (var userId in userIds)
+            {
+                var userResponses = signupsByResponse
+                    .Where(responseSignups => responseSignups.Value.Contains(userId))
+                    .Select(responseSignups => responseSignups.Key);
+
+                signupsByUser.Add(userId, userResponses);
+            }
+
+            return signupsByUser;
+        }
+
+        public static DiscordEmbedBuilder GetSignupEmbed(
+            DiscordEvent discordEvent,
+            Dictionary<EventResponse, IEnumerable<ulong>> signupsByResponse)
+        {
+            var signupsByUser = GetResponsesByUser(signupsByResponse);
+
+            var signupEmbed = new DiscordEmbedBuilder
+            {
+                Title = $"{discordEvent.Name} - {discordEvent.Time:ddd dd MMM yyyy @ h:mm tt}",
+                Description = discordEvent.Description,
+                Footer = new DiscordEmbedBuilder.EmbedFooter
+                {
+                    Text = $"event key: {discordEvent.Key}"
+                }
+            };
+
+            if (signupsByUser.Any())
+            {
+                var usersField = string.Join(
+                    "\n",
+                    signupsByUser.Select(response => $"<@{response.Key}>")
+                );
+
+                signupEmbed.AddField("Participants", usersField, true);
+
+                var responsesList = signupsByUser.Select(
+                    response => string.Join(" ", response.Value)
+                );
+                var responsesField = string.Join("\n", responsesList);
+
+                signupEmbed.AddField("Response(s)", responsesField, true);
+            }
+            else
+            {
+                signupEmbed.AddField("Participants", "There are currently no signups.");
+            }
+
+            var optionsField = string.Join(
+                "\n",
+                signupsByResponse.Select(response => $"{response.Key.Emoji} - {response.Key.ResponseName}")
+            );
+
+            signupEmbed.AddField("Response options", optionsField);
+
+            return signupEmbed;
+        }
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -2,11 +2,16 @@
 using System.Threading.Tasks;
 using DiscordBot.Commands;
 using DiscordBot.DataAccess;
+using DiscordBot.DataAccess.Exceptions;
+using DiscordBot.DataAccess.Models;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using static DiscordBot.Commands.EventHelper;
 
 namespace DiscordBot
 {
@@ -23,12 +28,14 @@ namespace DiscordBot
             {
                 Token = Environment.GetEnvironmentVariable("RELEASE_BOT_TOKEN"),
                 TokenType = TokenType.Bot,
-                Intents = DiscordIntents.AllUnprivileged 
-                          | DiscordIntents.GuildMembers
+                Intents = DiscordIntents.AllUnprivileged
+                          | DiscordIntents.GuildMembers,
+                MinimumLogLevel = LogLevel.Debug
             });
 
+            var eventsSheetsService = new EventsSheetsService();
             var services = new ServiceCollection()
-                .AddSingleton<IEventsSheetsService>(new EventsSheetsService())
+                .AddSingleton<IEventsSheetsService>(eventsSheetsService)
                 .BuildServiceProvider();
 
             var commands = discord.UseCommandsNext(new CommandsNextConfiguration
@@ -40,9 +47,84 @@ namespace DiscordBot
             commands.RegisterCommands<EventCommands>();
 
             discord.UseInteractivity(new InteractivityConfiguration());
+            discord.MessageReactionAdded += async (client, eventArguments) =>
+            {
+                await OnMessageReactionAdded(client, eventArguments, eventsSheetsService);
+            };
 
             await discord.ConnectAsync();
             await Task.Delay(-1);
+        }
+
+        private static async Task OnMessageReactionAdded(
+            DiscordClient client,
+            MessageReactionAddEventArgs eventArguments,
+            IEventsSheetsService eventsSheetsService)
+        {
+            // Skip if event was triggered by the bot
+            if (eventArguments.User == client.CurrentUser)
+            {
+                return;
+            }
+
+            var discordEvent = await GetEventFromMessageIdOrDefaultAsync(eventArguments.Message.Id, eventsSheetsService);
+            if (discordEvent == null)
+            {
+                return;
+            }
+
+            await ProcessReaction(client, eventArguments, discordEvent, eventsSheetsService);
+
+            await UpdateSignupSheet(eventArguments, discordEvent, eventsSheetsService);
+        }
+
+        private static async Task ProcessReaction(
+            DiscordClient client,
+            MessageReactionAddEventArgs eventArguments,
+            DiscordEvent discordEvent,
+            IEventsSheetsService eventsSheetsService)
+        {
+            var dmChannel = await eventArguments.Guild
+                .GetMemberAsync(eventArguments.User.Id).Result
+                .CreateDmChannelAsync();
+
+            if (eventArguments.Emoji.GetDiscordName() == ":no_entry_sign:")
+            {
+                await eventsSheetsService.ClearResponsesForUserAsync(discordEvent.Key, eventArguments.User.Id);
+                await client.SendMessageAsync(dmChannel, $"You've signed off {discordEvent.Name}.");
+            }
+            else
+            {
+                try
+                {
+                    await eventsSheetsService.AddResponseForUserAsync(
+                        discordEvent.Key,
+                        eventArguments.User.Id,
+                        eventArguments.Emoji.GetDiscordName());
+                }
+                catch (ResponseNotFoundException)
+                {
+                    return;
+                }
+
+                await client.SendMessageAsync(
+                    dmChannel,
+                    $"You've responded to {discordEvent.Name} as {eventArguments.Emoji.Name}.");
+            }
+        }
+
+        private static async Task UpdateSignupSheet(
+            MessageReactionAddEventArgs eventArguments,
+            DiscordEvent discordEvent,
+            IEventsSheetsService eventsSheetsService)
+        {
+            var signupsByResponse = await eventsSheetsService.GetSignupsByResponseAsync(discordEvent.Key);
+            await eventArguments.Message.ModifyAsync(
+                eventArguments.Message.Content,
+                GetSignupEmbed(discordEvent, signupsByResponse).Build()
+            );
+
+            await eventArguments.Message.DeleteReactionAsync(eventArguments.Emoji, eventArguments.User);
         }
     }
 }
